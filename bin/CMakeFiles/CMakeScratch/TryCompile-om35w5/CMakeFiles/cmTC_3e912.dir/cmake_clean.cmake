ec489c5de6f3eb4a4c6063d7c0441b357473c06d
Salvely <iamgwen9@gmail.com>
2024-09-11 11:53:46 +0800

    init github actions repo

---
+++ bin/CMakeFiles/CMakeScratch/TryCompile-om35w5/CMakeFiles/cmTC_3e912.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_3e912.dir/CMakeCCompilerABI.c.obj"
+  "cmTC_3e912.exe"
+  "cmTC_3e912.exe.manifest"
+  "cmTC_3e912.pdb"
+  "libcmTC_3e912.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_3e912.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
